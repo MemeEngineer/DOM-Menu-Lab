20a9774e5fa8169bfb64a94a2dbd22cfb7428142
JWu <johnnyw131@gmail.com>
2023-07-13 16:07:10 -0400

    Task 2

--- script.js
+++ script.js
@@ -2,4 +2,18 @@
 const mainEl = document.querySelector('main')
 console.log(mainEl)
 
+//====== 1.1
+
+mainEl.style.background= 'var(--main-bg)'
+
+//===== 1.2
+
+mainEl.innerHTML = '<h1>SEI ROCKS!</h1>'
+
+//===== 1.3
+
+mainEl.classList.add('flex-ctr')
+
+
+//==== task 2.0
 
